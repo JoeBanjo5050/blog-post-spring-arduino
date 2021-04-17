b2a123a6fc873f2b28da8b8124a46acac22d6b4a
Joe <leebrunovsky@gmail.com>
2021-04-17 15:37:13 +0900

    Add files via upload

--- springarduino/src/main/java/com/springarduino/SpringArduinoApplication.java
+++ springarduino/src/main/java/com/springarduino/SpringArduinoApplication.java
@@ -1,13 +1,18 @@
 package com.springarduino;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-public class SpringArduinoApplication {
+@Configuration
+@EnableAutoConfiguration
+
+public class SpringArduinoApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringArduinoApplication.class, args);
 	}
-
 }
+
